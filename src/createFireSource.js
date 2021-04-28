add4b00ca0ffd5180d4bee79c348322567cc931d
Yago Fontoura <yagofontorua222@gmail.com>
2021-04-27 21:52:48 -0300

    Revert "CalculateFirePropagation"
    This reverts commit 02f696c9c67028b7a8ef5c1107f7734898569c65.

--- src/createFireSource.js
+++
@@ -1,8 +0,0 @@
-export default function createFireSource(state.fireWidth,state.fireHeight) {
-            for (let column = 0; column <= state.fireWidth; column++) {
-                const overflowPixelIndex = state.fireWidth * state.fireHeight
-                const pixelIndex = (overflowPixelIndex - fireWidth) + column
-
-                state.firePixelsArray[pixelIndex] = 35
-            }
-        }
